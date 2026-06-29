d3de63b057f32efa170b2c475d13680233f355f9
akash0x00 <145096386+akash0x00@users.noreply.github.com>
2026-06-29 18:09:28 +0530

    Add files via upload

--- install-zsh.sh
+++ install-zsh.sh
@@ -146,10 +146,10 @@ bindkey -M vicmd 'j' history-substring-search-down
 
 # ---- Listing aliases (eza if present, else coloured coreutils ls) ---------
 if command -v eza >/dev/null 2>&1; then
-  alias ls='eza --group-directories-first --icons=auto'
-  alias ll='eza -lh --group-directories-first --icons=auto --git'
-  alias la='eza -lah --group-directories-first --icons=auto --git'
-  alias lt='eza --tree --level=2 --group-directories-first --icons=auto'
+  alias ls='eza --group-directories-first'
+  alias ll='eza -lh --group-directories-first --git'
+  alias la='eza -lah --group-directories-first --git'
+  alias lt='eza --tree --level=2 --group-directories-first'
 else
   alias ls='ls --color=auto --group-directories-first'
   alias ll='ls -lh --color=auto --group-directories-first'
